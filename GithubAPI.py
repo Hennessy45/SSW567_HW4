c99ea3b3cdd4bc4ab6cd233527eec24348af5894
Hennessy <htorres2@stevens.edu>
2022-03-20 22:32:21 -0400

    first commit

---
+++ GithubAPI.py
@@ -0,0 +1,22 @@
+import unittest
+from github import findGithubInfo
+
+
+class GithubAPI(unittest.TestCase):
+    def testInvalidUsername(self):
+        self.assertEqual(findGithubInfo("jfkdjshfksjdhfkjsdfjsdhfkjsh"), "Invalid username entered. Please re-run the program and try again.")
+    def testValidUsername(self):
+        self.assertTrue(findGithubInfo("nbernhard314").__contains__("Repo: covidhack, Number of commits: 2\nRepo: cs554, Number of commits: 4\nRepo: cs555, Number of commits: 78\n"))
+    def testBoolInput(self):
+        self.assertEqual(findGithubInfo(False), "Invalid input. Please enter a string.")
+    def testIntInput(self):
+        self.assertEqual(findGithubInfo(3), "Invalid input. Please enter a string.")
+    def testFloatInput(self):
+        self.assertEqual(findGithubInfo(12.52), "Invalid input. Please enter a string.")
+    def testNoneInput(self):
+        self.assertEqual(findGithubInfo(None), "Invalid input. Please enter a string.")
+
+
+if __name__ == '__main__':
+    print('Running unit tests')
+    unittest.main()
---
+++ HW4A.py
@@ -0,0 +1,34 @@
+import requests as r
+import json
+
+def findGithubInfo(username):
+
+    if not isinstance(username, str):
+        return "Invalid input. Please enter a string."
+
+    ret = r.get("https://api.github.com/users/"+username+"/repos")
+
+    if not ret.ok:
+        return "Invalid username entered. Please re-run the program and try again."
+
+    ret=ret.json()
+
+    repoList = []
+    for obj in ret:
+        repoList.append(obj["name"])
+
+    output = ""
+    for repo in repoList:
+
+        comRes = r.get("https://api.github.com/repos/"+username+"/"+repo+"/commits?page=1&per_page=1000")
+
+        comRes = comRes.json()
+
+        output+=("Repo: "+repo+", Number of commits: "+str(len(comRes))+"\n")
+    return output
+
+if __name__ == '__main__':
+
+    username = input("Please enter a Github username: ")
+
+    print(findGithubInfo(username))
